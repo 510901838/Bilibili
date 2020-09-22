6ff219036d797f8c9aeba9486fefaff1b98ea001
Kevin <510901838@qq.com>
2020-09-22 21:04:14 +0800

    Initial commit

---
+++ 频道/js/back-top.js
@@ -0,0 +1,15 @@
+//隐藏和显示
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 300) {
+		$('.back-top-btn').fadeIn();
+	} else {
+		$('.back-top-btn').fadeOut();
+	}
+});
+//点击跳转到顶部
+$('.back-top-btn').click(function() {
+	$('html ,body').animate({
+		scrollTop: 0
+	}, 300);
+	return false;
+});
